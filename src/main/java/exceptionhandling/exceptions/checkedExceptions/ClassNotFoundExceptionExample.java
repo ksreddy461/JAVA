483621de9fede37a9f36d03aef42814ad68fb0d3
Sreedhara Reddy Kodadala <skodadala@altimetrik.com>
2019-03-11 18:31:25 +0530

    Added SQL & ClassNotFound Exception Examples

---
+++ src/main/java/exceptionhandling/exceptions/checkedExceptions/ClassNotFoundExceptionExample.java
@@ -0,0 +1,16 @@
+package exceptionhandling.exceptions.checkedExceptions;
+
+public class ClassNotFoundExceptionExample {
+
+    private static final String CLASS_TO_LOAD = "main.java.utils";
+
+    public static void main(String[] args) {
+        try {
+            Class loadedClass = Class.forName(CLASS_TO_LOAD);
+            System.out.println("Class " + loadedClass + "found Successfully");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Got Exception");
+            e.printStackTrace();
+        }
+    }
+}
